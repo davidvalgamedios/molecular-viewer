ddb000f44f6f284c404a67bf34e557835b2df0ed
David Valero <dvdvalero89@gmail.com>
2017-03-09 23:58:56 +0100

    Now a working prod build!

--- rollup-config.js
+++ rollup-config.js
@@ -14,7 +14,7 @@ export default {
         // should intercept ... but doesn't in some rollup versions
         if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
         // intercepts in some rollup versions
-        if ( warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+        if ( warning.message.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
 
         // console.warn everything else
         console.warn( warning.message );
@@ -22,7 +22,10 @@ export default {
     plugins: [
         nodeResolve({jsnext: true, module: true}),
         commonjs({
-            include: 'node_modules/rxjs/**'
+            include: 'node_modules/rxjs/**',
+            namedExports: {
+                'node_modules/three/examples/js/loaders/PDBLoader.js':['PDBLoader', 'THREE.PDBLoader']
+            }
         }),
         uglify()
     ]
