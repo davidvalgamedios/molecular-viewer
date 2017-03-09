34e67b3346b2c563981cac95879f80cdf71f14db
David Valero <dvdvalero89@gmail.com>
2017-03-09 20:03:47 +0100

    More explanation texts

--- site/pages/editor-page.component.ts
+++ site/pages/editor-page.component.ts
@@ -21,14 +21,25 @@ import {ProjectService} from "../services/project.service";
             </div>
             
             <div *ngIf="isNewEmptyProject()" class="explanation flxCntr">
-                <div class="big">¡Hola! Parece que este proyecto está vacio.</div>
+                <div class="big">Parece que este proyecto está vacio.</div>
                 <div class="med">¿Que te parece si empezamos por añadir un fondo?</div>
                 <div class="action">
                     <div class="btn" (click)="openBackgroundSelection()">
                         Añadir fondo <i class="fa fa-plus-circle"></i>
                     </div>
                 </div>
-                <div class="small">(Podrás cambiarlo más tarde si no te gusta)Psst. Todos tus cambios se guardarán automáticamente en tu ordenador. Solo recuerda guardar antes de irte.</div>
+                <div class="small">(Podrás cambiarlo más tarde si no te gusta)</div>
+            </div>
+            
+            <div *ngIf="isNewEmptyProject()" class="explanation flxCntr">
+                <div class="big">¡Genial!</div>
+                <div class="med">Ahora que ya tienes un fondo ¿Añadimos algunas moléculas?</div>
+                <div class="action">
+                    <div class="btn" (click)="openBackgroundSelection()">
+                        Añadir moléculas <i class="fa fa-plus-circle"></i>
+                    </div>
+                </div>
+                <div class="small">Psst. Todos tus cambios se guardarán automáticamente en tu ordenador. Solo recuerda guardar antes de irte.</div>
             </div>
         </div>
         <!--<footer></footer>-->
