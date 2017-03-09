0077571e2838b3468bb8b1f3fdc1574aba58193c
David Valero <dvdvalero89@gmail.com>
2017-03-09 20:09:47 +0100

    Added saving and restoring to localStorage

--- site/services/project.service.ts
+++ site/services/project.service.ts
@@ -2,24 +2,40 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ProjectService {
+    private projectId:string;
     private projectCfg:any;
 
     constructor(){
-        this.projectCfg = {
-            projectName: 'Proyecto de prueba',
-            background: null
+        this.projectId = 'demo';
+
+        let savedData = localStorage.getItem('prj-'+this.projectId);
+        if(savedData){
+            this.projectCfg = JSON.parse(savedData);
+        }
+        else{
+            this.projectCfg = {
+                projectName: 'Proyecto de prueba',
+                background: null
+            }
         }
     }
 
-    getProjectConfig(){
+    getProjectConfig():any{
         return this.projectCfg;
     }
 
     updateProjectName(newName:string){
         this.projectCfg.projectName = newName;
+        this.saveChangesLocally();
     }
 
     updateBackground(backgroundId:string){
         this.projectCfg.background = backgroundId;
+        this.saveChangesLocally();
+    }
+
+
+    private saveChangesLocally():void{
+        localStorage.setItem('prj-'+this.projectId, JSON.stringify(this.projectCfg));
     }
 }
